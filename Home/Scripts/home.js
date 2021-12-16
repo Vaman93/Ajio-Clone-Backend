273540f59873bc27de9e25204f988849a7a6e764
Atharva-s93 <athavaganbote@gmail.com>
2021-12-16 11:58:59 +0530

    add

--- Home/Scripts/home.js
+++ Home/Scripts/home.js
@@ -1,10 +1,4 @@
-import {navbarCom , footershow} from "/Home/components/navbar_footer.js"
 
-let mainnavbar = document.getElementById("Nav_bar_main")
-    mainnavbar.innerHTML = navbarCom()
-
-let footershowinhtml = document.querySelector("footer")
-    footershowinhtml.innerHTML = footershow()
  
     
 $('.single-item').slick({
@@ -58,24 +52,3 @@ heding_img_arr.forEach((d)=>{
     headingimgnum++
 })
 
-
-
-// Navbar Js 
-let hidethenavbar = document.querySelector(".input_serach_div_secon_nav")
-
-let navbar = document.querySelector("#hovermain")
-
-let showthesuggestion = document.querySelector(".input_suggestion_div")
-
-document.querySelector("body").addEventListener("wheel", hidediv)
-
-hidethenavbar.addEventListener("click", hide)
-
-function hide() {
-  showthesuggestion.style.display = "block"
-}
-
-
-function hidediv(){
-  showthesuggestion.style.display = "none"
-}
