eef5d31adb82811b1cfbdcf83faa1681a4cdeb98
Atharva-s93 <athavaganbote@gmail.com>
2021-12-17 10:11:31 +0530

    h

--- EachProduct/scripts/main.js
+++ EachProduct/scripts/main.js
@@ -20,7 +20,16 @@ data=[
 ]
 
 
-
+$('.responsive').slick({
+    arrows: true,
+    dots: false,
+    infinite: true,
+    speed: 600,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    
+  });
+  
 
 
 
@@ -38,12 +47,12 @@ let OUR_PROMISE = document.querySelector(".OUR_PROMISE > p")
 
 function  showtheRetuernText() {
     returnsugggetion.innerHTML = `<p>Easy 15 days return and exchange. Return Policies may vary based on products and promotions. For full details on our Returns Policies, please <a href="">click here.</a> </p>`
-    ret.style.border="thin solid black"
+    ret.style.border="thin solid #ddd"
     OUR_PROMISE.style.border= "none"
 }
 function showtheOUR_PROMISE(){
     returnsugggetion.innerHTML =`We assure the authenticity and quality of our products
     `
-    OUR_PROMISE.style.border="thin solid black";
+    OUR_PROMISE.style.border="thin solid #ddd";
     ret.style.border= "none"
 }
