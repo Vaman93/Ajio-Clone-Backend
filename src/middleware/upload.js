70fb847ba304257f82816f023fb5acdadb3f5c10
Atharva-s93 <athavaganbote@gmail.com>
2022-01-18 15:51:14 +0530

    upadte

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -2,7 +2,7 @@ const path = require("path")
 const multer  = require('multer')
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, path.join(__dirname , "../../public"))
+      cb(null, "upload")
     },
     filename: function (req, file, cb) {
       const unique = Date.now() + '-' + Math.round(Math.random() * 1E9)
@@ -21,9 +21,9 @@ const storage = multer.diskStorage({
 
 const upload = multer({
     storage: storage,
-    // limits: {
-    //   fileSize: 1024 * 1024 * 5,
-    // },
+    limits: {
+      fileSize: 1024 * 1024 * 5,
+    },
 });
 
 module.exports = upload;
