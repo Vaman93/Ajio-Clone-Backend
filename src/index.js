ddfb83b3b103b1944a63126ca3f44257e1e64317
sarang999 <sarangchaudhary2014@gmail.com>
2022-01-18 21:06:44 +0530

    index_updated

--- src/index.js
+++ src/index.js
@@ -18,8 +18,25 @@ app.use("/productadd" , ProductAddSchema)
 app.use("/product" , ProductAPI)
 
 
-app.get("/productjackets" , (req, res) => {
-  res.render("product page/productjackets");
-})
+// app.get("/productjackets", (req, res) => {
+//   res.render("product page/productjackets");
+// });
+
+// app.get("/productjeans", (req, res) => {
+//   res.render("product page/productjeans");
+// });
+
+// app.get("/productwomenjeans", (req, res) => {
+//   res.render("product page/productwomenjeans");
+// });
+
+// app.get("/productwomentshirt", (req, res) => {
+//   res.render("product page/productwomentshirt");
+// });
+
+
+
+
+
 
 module.exports = app;
--- src/router/product.api.js
+++ src/router/product.api.js
@@ -3,23 +3,47 @@ const router = express.Router();
 
 const ProductSchema = require('../Schema/project.schema')
 
+
+//men jacket and jeans 
 router.get("/men/productjacket" , async (req, res) => {
     try{
-        const produnct = await ProductSchema.find({producttype: {$eq:"jacket"}})
-        res.send(produnct)
+        const product = await ProductSchema.find({ producttype: { $eq: "jacket" } });
+        res.send(product);
+
     }catch (err) {
-        console.log(err)
+        console.log(err);
     }
 })
 
-router.get("/men/productjeans" , async (req, res) => {
+router.get("/men/productjeans", async (req, res) => {
+    try {
+        const product = await ProductSchema.find({ producttype: { $eq: "jeans" }, gender: { $eq: "male" } });
+        res.send(product);
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+// women jeans and tshirt
+
+router.get("/women/producttshirt" , async (req, res) => {
     try{
-        const produnct = await ProductSchema.find({producttype: {$eq:"jeans"} , gender: {$eq:"male"} })
-        res.send(produnct)
+        const product = await ProductSchema.find({ producttype: { $eq: "tshirt" } });
+        res.send(product);
+
     }catch (err) {
-        console.log(err)
+        console.log(err);
     }
 })
+
+router.get("/men/productjeans", async (req, res) => {
+    try {
+        const product = await ProductSchema.find({ producttype: { $eq: "jeans" }, gender: { $eq: "male" } });
+        res.send(product);
+    } catch (err) {
+        console.log(err);
+    }
+});
 
 
 module.exports = router
