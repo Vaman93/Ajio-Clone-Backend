6f926aa2c56a83109de4c7c9ef9b8b3242cbf271
Atharva-s93 <athavaganbote@gmail.com>
2022-01-18 16:03:03 +0530

    upadte

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const ProductAddSchema = require('./router/product.add')
 app.set('view engine', 'ejs')
 app.use(express.static("public"))
 app.use("/upload", express.static("./upload"))
-console.log(app.use(express.static("./upload")))
+
 app.use(express.json())
 
 app.use("/" , Homepage)
--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -5,22 +5,23 @@ const storage = multer.diskStorage({
       cb(null, "upload")
     },
     filename: function (req, file, cb) {
-      const unique = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null,  unique + '-' + file.fieldname)
+      const unique = Date.now() + '-' + Math.round(Math.random() * 1E9) + path.extname(file.originalname)
+      cb(null,  unique)
     }
   })
 
-// const fileFilter = (req, file, callback) => {
-//     if (file.mimetype === "image/png" || file.mimetype === "image/jpeg" || file.mimetype === "image/jpg") {
-//       callback(null, true);
-//     } else {
+const fileFilter = (req, file, callback) => {
+    if (file.mimetype === "image/png" || file.mimetype === "image/jpeg" || file.mimetype === "image/jpg") {
+      callback(null, true);
+    } else {
   
-//       callback(null, false);
-//     }
-//   };
+      callback(null, false);
+    }
+  };
 
 const upload = multer({
     storage: storage,
+    
     limits: {
       fileSize: 1024 * 1024 * 5,
     },
