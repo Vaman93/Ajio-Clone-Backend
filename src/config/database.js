2c885a308aeb8bc918da6621af4e3fb545e3ac5a
Atharva-s93 <athavaganbote@gmail.com>
2022-01-18 11:04:39 +0530

    a

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,7 @@
-const dotenv = require('dotenv')
-dotenv.config()
 const {connect} = require('mongoose')
 
 const DP = `mongodb+srv://vaman123:vamanAWS@cluster0.hmu5h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 
 module.exports = () => {
-    return connect(DP ,{useUnifiedTopology: true,
-        useNewUrlParser: true,})
+    return connect(DP)
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 const express = require('express')
-const path = require('path');
 const app = express()
 
 app.set('view engine', 'ejs')
-app.use( express.static("public"));
-
+app.use(express.static("public"))
 
 app.get("/" , (req, res) => {
   res.render("index")
